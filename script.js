5369cc6fda3c4b7c99a45d82d2f2ab4ac50ce3ee
Ian Clark <ianclark226@gmail.com>
2019-12-18 16:16:50 -0500

    12/18

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-$(document).ready(function() {
-    $(".saveBtn").on("click", function(){
-        console.log(this);
-        var value = $(this).siblings(".description").val();
-        
-        var time = $(this).parent().attr(".id");
-
-        localStorage.setItem(time, value);
+$(document).ready(function(){
+    $(".saveBtn").on("click", function() {
+      console.log(this);
+      var value = $(this).siblings(".description").val();
+      // Use this
+      // var time = $(this).siblings(".hour").text();
+  
+      // Or this
+      var time = $(this).parent().attr("id");
+  
+      localStorage.setItem(time, value);
     });
-$("#currentDay").text(moment());
 
-}); 
+     setInterval(function() {
+
+    
+    $("#currentDay").text(moment());
+
+     }, 1000);
+    
+  });
+
+
+
+
+var hour = moment().format('H');
+
+
+
+
+
+$(".description").each(function (index, el) {
+
+    if (hour == (index + 9)) {
+
+        $(el).css('backgroundColor', "#ff6961");
+    }
+    
+      if (hour < (index + 9)) {
+         $(el).css('backgroundColor', "#77dd77");
+      }
+         if (hour >(index + 9 )) {
+         $(el).css('backgroundColor', "#d3d3d3");
+     }
+});
+
+
 
 
 
