327aa43909e9c7bd271346e43544e0bb6ee07892
Ian Clark <ianclark226@gmail.com>
2019-12-19 18:22:05 -0500

    12/19

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Homework_05
+
+Here is my day planner.  At the top of the html, displays the title, day, and time. One thing different from this planner than from the example is that it has the time and seconds counting down. It has hours 9am-5pm which is the average work day. What ever the curren hour is of the day, the textarea will be red. If the hours are in the future of the current hour, they will be green. If the hours are in the past, they will be gray. When you type out something in one of the textareas, you hit the save button to save it to local storage.
--- script.js
+++ script.js
@@ -30,7 +30,10 @@ $(document).ready(function(){
 
   
 
-  
+//    var saveText = $(".description").val();
+// // //get elements by class name which is the "description" and then save that text
+//        document.getElementsByClassName('.description').innerHTML;
+      
 
     
 
@@ -56,6 +59,21 @@ $(".description").each(function (index, el) {
          $(el).css('backgroundColor', "#d3d3d3");
      }
 });
+
+function saveValue(e){
+  var id = e.id;  // get the sender's id to save it . 
+  var val = e.value; // get the value. 
+  localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+}
+
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue  (v){
+  if (!localStorage.getItem(v)) {
+      return "";// You can change this to your defualt value. 
+  }
+  return localStorage.getItem(v);
+}
+
 
 
 
